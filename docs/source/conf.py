8470d7aebbe27973af70a191f1f4e1649bf0743b
bergsmith <ryguydev@gmail.com>
2022-06-20 15:25:15 -0500

    docs: fix autodocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../csvcompare'))
+sys.path.insert(0, os.path.abspath('../../csvcomparer'))
 
 # -- Project information -----------------------------------------------------
 
